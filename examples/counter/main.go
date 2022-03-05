209fb0c82290782e8b0cfbccbf1c7102df6b7ba5
Simon Sun <sun.zg@outlook.com>
2022-03-05 19:38:19 +0800

    update

--- examples/counter/main.go
+++ examples/counter/main.go
@@ -11,7 +11,7 @@ import (
 	"runtime"
 	"sync"
 
-	"github.com/zserge/lorca"
+	"github.com/kutogroup/lorca"
 )
 
 //go:embed www
--- examples/hello/main.go
+++ examples/hello/main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net/url"
 
-	"github.com/zserge/lorca"
+	"github.com/kutogroup/lorca"
 )
 
 func main() {
